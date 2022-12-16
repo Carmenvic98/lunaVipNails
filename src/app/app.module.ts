78035ee1357c501499517227b056d8a13dc5fbef
Carmenvic98 <carmenv.rios.cr@gmail.com>
2022-12-16 18:20:24 -0500

    componentes creados e iniciando el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    AboutUsComponent,
+    ListComponent,
+    DetailsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
