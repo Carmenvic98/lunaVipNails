2e4f3099685c2f8602154cab40246f7a8c74651b
Carmenvic98 <carmenv.rios.cr@gmail.com>
2022-12-20 15:37:30 -0500

    header funcional y responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor(){}
+  ngOnInit(): void {
+    var menu =document.getElementsByClassName("bar")[0];
+    var nav =document.getElementsByClassName("navegador")[0]
+
+    menu.addEventListener("click", function(){
+      nav.classList.toggle('active') 
+    })
+  }
 
 }
